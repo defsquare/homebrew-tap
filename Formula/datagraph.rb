a212020d9b183beca349251195eb22a169c35b03
davidpanza <david.panza@gmail.com>
2026-09-15 11:01:17 +0200

    datagraph 0.1.2

--- Formula/datagraph.rb
+++ Formula/datagraph.rb
@@ -1,9 +1,9 @@
 class Datagraph < Formula
   desc     "Explore a JSON document as an interactive graph of records and references"
   homepage "https://github.com/defsquare/datagraph"
-  url      "https://dl.datagraph.defsquare.com/datagraph/datagraph-0.1.1-darwin-arm64.tar.gz"
-  version  "0.1.1"
-  sha256   "415889fca6a19e0172b1aa5294d2fdc01e19504d9e4a4d59b96d6f1e8ed3a5f5"
+  url      "https://dl.datagraph.defsquare.com/datagraph/datagraph-0.1.2-darwin-arm64.tar.gz"
+  version  "0.1.2"
+  sha256   "66c823dbcfab91d087c4149ea461ef811e5a435caef98ed533adb060361bcd1c"
   license  "MIT"
 
   depends_on arch: :arm64
